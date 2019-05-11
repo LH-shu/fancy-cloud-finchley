001260ac2fa73646928612c651a6955474cb91ec
lihuan <SmartShuShu@163.com>
2019-05-11 21:11:42 +0800

    update

--- cloud-finchley-config/src/main/java/pers/fancy/cloud/finchley/ConfigApplication.java
+++ cloud-finchley-config/src/main/java/pers/fancy/cloud/finchley/ConfigApplication.java
@@ -1,4 +1,4 @@
-package pers.fancy.cloud.finchley.consumer;
+package pers.fancy.cloud.finchley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
